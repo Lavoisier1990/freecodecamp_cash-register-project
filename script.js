3744170ca154eaed7f858a526d07d9c57c67ad25
Takunda Kangai <kangaitakunda@gmail.com>
2024-09-21 02:10:29 +0200

    Modify script. Initialise the constants for the keypad and other intital objects of the application.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const customerCash = document.getElementById("cash");
+const purchaseTotal = document.getElementById("price-screen");
+const resultsDiv = document.getElementById("change-due");
+const oneBtn = document.getElementById("one-btn");
+const twoBtn = document.getElementById("two-btn");
+const threeBtn = document.getElementById("three-btn");
+const fourBtn = document.getElementById("four-btn");
+const fiveBtn = document.getElementById("five-btn");
+const sixBtn = document.getElementById("six-btn");
+const sevenBtn = document.getElementById("seven-btn");
+const eightBtn = document.getElementById("eight-btn");
+const nineBtn = document.getElementById("nine-btn");
+const dotBtn = document.getElementById("dot-btn");
+const zeroBtn = document.getElementById("zero-btn");
+const clearBtn = document.getElementById("clear-btn");
+const purchaseBtn = document.getElementById("purchase-btn");
+
+
+
 let price = 1.87;
 let cid = [
   ['PENNY', 1.01],
